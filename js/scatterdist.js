fb78884ccb9679bb807035d015f6fe6f7783312d
Kevin (Taeyoon) Jin <kevin@phantomore.com>
2020-11-24 21:53:39 -0500

    [Intermediate] Styles fixes

--- js/scatterdist.js
+++ js/scatterdist.js
@@ -6,285 +6,268 @@ function scatterplot() {
     // Based on Mike Bostock's margin convention
     // https://bl.ocks.org/mbostock/3019563
     let margin = {
-        top: 60,
-        left: 50,
-        right: 30,
-        bottom: 40
-      },
-      width = 400 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom,
-      xValue = data => data.timespent,
-      yValue = data => (data.score/ data.totalPoints) * 100,
-      xLabelText = '',
-      yLabelText = '',
-      yLabelOffsetPx = 0,
-      xScale = d3.scaleLinear(),
-      yScale = d3.scaleLinear(),
-      ourBrush = null,
-      selectableElements = d3.select(null),
-      dispatcher;
+            top: 60,
+            left: 50,
+            right: 30,
+            bottom: 40
+        },
+        width = 500 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom,
+        xValue = data => data.timespent,
+        yValue = data => (data.score/ data.totalPoints) * 100,
+        xLabelText = '',
+        yLabelText = '',
+        yLabelOffsetPx = 0,
+        xScale = d3.scaleLinear(),
+        yScale = d3.scaleLinear(),
+        ourBrush = null,
+        selectableElements = d3.select(null),
+        dispatcher;
 
-    
+
     // Below are the basic D3 principles applied to make a scatterplot
-      
+
     function chart(selector, data) {
-    
-    //For timespent it is only applicable for quiz and offline quiz
-    data = data.filter(function(d){return d.assignType === "quiz" || d.assignType === "offline-quiz"} )
 
-    let svg = d3.select(selector)
-        .append('svg')
-          .attr('preserveAspectRatio', 'xMidYMid meet')
-          .attr('viewBox', [0, 0, width + margin.left + margin.right, height*2.5].join(' '))
-        
+        //For timespent it is only applicable for quiz and offline quiz
+        data = data.filter(function(d){return d.assignType === "quiz" || d.assignType === "offline-quiz"} )
 
-    //Title Text
-      svg.append("text")
-          .attr("x", (width / 2 + margin.right + margin.left))             
-          .attr("y", 0 + (height - margin.top - margin.bottom))
-          .attr("text-anchor", "middle")  
-          .style("font-size", "30px") 
-          .text("Grades by Time Spent"); 
-    // Transition Text
-    svg.append("text")
-          .attr("x", (width / 2 + margin.right + margin.left))             
-          .attr("y", 0 + (height*3 - margin.top - margin.bottom))
-          .attr("text-anchor", "middle")  
-          .style("font-size", "20px") 
-          .text("Below are the reference distributions from the chart above");
-    
-    svg = svg.append('g')
-          .attr('transform', 'translate(' + width/3 + ',' + (height) + ')');
-    
+        let svg = d3.select(selector)
+            .append('svg')
+            .attr('preserveAspectRatio', 'xMidYMid meet')
+            .attr('viewBox', [0, -50, width, height + 175].join(' '))
 
-    let legend = svg.append('g')
-          .attr('transform', 'translate(' + -width/1.1 + ',' + ((height - margin.bottom- margin.top)/2) + ')');
-    
-    //Setting up the sidescaling distributions
-    let dist = svg.append('g')
-    .attr('transform', 'translate(' + 0  + ',' + -(height + 10) + ')')
-    
-          
-    // NOTE THE SECOND HISTOGRAM IS ROTATED 90 Degrees
-    let seconddist = svg.append('g') 
-    .attr('transform', 'translate(' + (width+height + margin.right) + ',' + 0 + ')' +  'rotate(90)')
-          .attr('class', 'gRotate')
-  
+        //Title Text
+        svg.append("text")
+            .attr("x", (500 / 2))
+            .attr("y", -30)
+            .attr("text-anchor", "middle")
+            .style("font-size", "21px")
+            .text("Grades by Time Spent");
+
+        svg = svg.append('g')
+            .attr('transform', 'translate(' + 0 + ',' + 0 + ')');
+
+
+        let legend = svg.append('g')
+            .attr('transform', 'translate(' + 0 + ',' + 450 + ')');
+
         xScale
-          .domain(d3.extent(data, xValue)) // Use extent to get the min, max
-          .rangeRound([0, width]);
+            .domain(d3.extent(data, xValue)) // Use extent to get the min, max
+            .rangeRound([0, width]);
 
         yScale
-          .domain(d3.extent(data, yValue))
-          .rangeRound([height, 0]);
+            .domain(d3.extent(data, yValue))
+            .rangeRound([height, 0]);
 
-    
+
         let xAxis = svg.append('g')
             .attr('transform', 'translate(0,' + (height) + ')')
             .call(d3.axisBottom(xScale));
-            
+
         // X axis label
-        xAxis.append('text')        
+        xAxis.append('text')
             .attr('class', 'axisLabel')
             .attr('y', 35)
             .attr('fill', 'black')
-            .style("font-size", "15px") 
+            .style("font-size", "15px")
             .attr('x', (width) / 2)
             .text(xLabelText); //xLabelText is applied in the main visualization.js file
 
-          
+
         let yAxis = svg.append('g')
             .call(d3.axisLeft(yScale))
-            
+
         yAxis.append('text')
             .attr('class', 'axisLabel')
             .attr('y', -25)
-            .attr('fill', 'black') 
+            .attr('fill', 'black')
             .attr('x', -height/2)
             .attr('transform', `rotate(-90)`)
             .attr('text-anchor', 'middle')
-            .style("font-size", "15px") 
+            .style("font-size", "15px")
             .text(yLabelText);//yLabelText is applied in the main visualization.js file
 
-          var colorScale = d3.scaleOrdinal(d3.schemeCategory10)
-          .domain(data.map(function (d){ return d.className; })); //ColorScheme set up based on class name
-          
+        var colorScale = d3.scaleOrdinal(d3.schemeCategory10)
+            .domain(data.map(function (d){ return d.className; })); //ColorScheme set up based on class name
+
         let points = svg.append('g')
-          .selectAll('.scatterPoint')
+            .selectAll('.scatterPoint')
             .data(data);
 
         points.exit().remove();
 
         points = points
             .enter().append('circle')
-              .attr('cy', d => yScale(yValue(d))) //Scale the y datapoint 
-              .attr('cx', d => xScale(xValue(d))) //Scale the x datapoint
-              .attr('r', 4)
-              .style("stroke", function(d) { //Color logic for the scatterplot points
+            .attr('cy', d => yScale(yValue(d))) //Scale the y datapoint
+            .attr('cx', d => xScale(xValue(d))) //Scale the x datapoint
+            .attr('r', 4)
+            .style("stroke", function(d) { //Color logic for the scatterplot points
                 return colorScale(d.className)
-                })
-    
-    
+            })
+
+
         let statusMap = new Map();   // https://observablehq.com/@d3/d3v6-migration-guide
-            data.forEach(d => {       // Using the migration guide to apply d3.Map in V6
+        data.forEach(d => {       // Using the migration guide to apply d3.Map in V6
             if (!statusMap.has(d.className)) statusMap.set(d.className, d);  // Add if not already present
-            }); //StatusMap contians a map iterator with unique values of d.status and its entire data fields
+        }); //StatusMap contians a map iterator with unique values of d.status and its entire data fields
 
         //https://www.d3-graph-gallery.com/graph/custom_legend.html inspiration for legend
         legend.selectAll("mydots")
             .data(statusMap.values()) //Insert the mapped values which we are concerned with, the status value will be distinct for each data object
             .enter()
             .append("circle")
-              .attr("cx", margin.left - 30)
-              .attr("cy", function(d,i){ return i*25}) // 100 is where the first dot appears. 25 is the distance between dots
-              .attr("r", 5)
-              .style("stroke", function(d) {
+            .attr("cx", margin.left - 30)
+            .attr("cy", function(d,i){ return i*15}) // 100 is where the first dot appears. 25 is the distance between dots
+            .attr("r", 3)
+            .style("stroke", function(d) {
                 return colorScale(d.className)
-                });
-                
+            });
+
         legend.selectAll("mylabels")
             .data(statusMap.values()) //Insert the mapped values which we are concerned with, the status value will be distinct for each data object
             .enter()
             .append("text")
             .attr("x", margin.left - 20)
-            .attr("y", function(d,i){ return i*25}) // 100 is where the first dot appears. 25 is the distance between dots
+            .attr("y", function(d,i){ return i*15}) // 100 is where the first dot appears. 25 is the distance between dots
             .text(function(d){ return d.className})
             .attr("text-anchor", "left")
             .style("alignment-baseline", "middle")
+            .style("font-size", "12px")
             .style("fill", function(d) { //Color logic for the scatterplot points
-            console.log(d)
-              return colorScale(d.className)
+                console.log(d)
+                return colorScale(d.className)
             })
-            
 
-    selectableElements = points;
-    
-    svg.call(brush);
 
-    // Highlight points when brushed
-    function brush(g) {
-      const brush = d3.brush() // Create a 2D interactive brush
-        .on('start brush', highlight) // When the brush starts/continues do...
-        .on('end', brushEnd) // When the brush ends do...
-        .extent([
-          [-margin.left, -margin.bottom],
-          [width + margin.right, height + margin.top]
-        ]);
-        
-      ourBrush = brush;
+        selectableElements = points;
 
-      g.call(brush); // Adds the brush to this element
+        svg.call(brush);
 
-      // Highlight the selected circles
-      function highlight(event, d) {
-        if (event.selection === null) return;
-        const [
-          [x0, y0],
-          [x1, y1]
-        ] = event.selection;
+        // Highlight points when brushed
+        function brush(g) {
+            const brush = d3.brush() // Create a 2D interactive brush
+                .on('start brush', highlight) // When the brush starts/continues do...
+                .on('end', brushEnd) // When the brush ends do...
+                .extent([
+                    [-margin.left, -margin.bottom],
+                    [width + margin.right, height + margin.top]
+                ]);
 
-        // If within the bounds of the brush, select it
-        points.classed('selected', d =>
-          x0 <= X(d) && X(d) <= x1 && y0 <= Y(d) && Y(d) <= y1
-        )
+            ourBrush = brush;
 
-        // Get the name of our dispatcher's event
-        let dispatchString = Object.getOwnPropertyNames(dispatcher._)[0];
-        // Let other charts know about our selection
-        console.log(svg.selectAll('.selected'))
-        dispatcher.call(dispatchString, this, svg.selectAll('.selected').data());
-      }
-      
-      function brushEnd(event, d){
-        // We don't want infinite recursion
-        if(event.sourceEvent !== undefined && event.sourceEvent.type!='end'){
-          d3.select(this).call(brush.move, null);
+            g.call(brush); // Adds the brush to this element
+
+            // Highlight the selected circles
+            function highlight(event, d) {
+                if (event.selection === null) return;
+                const [
+                    [x0, y0],
+                    [x1, y1]
+                ] = event.selection;
+
+                // If within the bounds of the brush, select it
+                points.classed('selected', d =>
+                    x0 <= X(d) && X(d) <= x1 && y0 <= Y(d) && Y(d) <= y1
+                )
+
+                // Get the name of our dispatcher's event
+                let dispatchString = Object.getOwnPropertyNames(dispatcher._)[0];
+                // Let other charts know about our selection
+                console.log(svg.selectAll('.selected'))
+                dispatcher.call(dispatchString, this, svg.selectAll('.selected').data());
+            }
+
+            function brushEnd(event, d){
+                // We don't want infinite recursion
+                if(event.sourceEvent !== undefined && event.sourceEvent.type!='end'){
+                    d3.select(this).call(brush.move, null);
+                }
+            }
         }
-      }
+
+        return chart;
     }
 
-      return chart;
-    }
-    
 
     // Below are the changeable chart elements based on the resuable model
     // Not all are being called on currently
     // The x-accessor from the datum
 
     function X(d) {
-      return xScale(xValue(d));
+        return xScale(xValue(d));
     }
-  
+
     // The y-accessor from the datum
     function Y(d) {
-      return yScale(yValue(d));
+        return yScale(yValue(d));
     }
-  
+
     chart.margin = function (_) {
-      if (!arguments.length) return margin;
-      margin = _;
-      return chart;
+        if (!arguments.length) return margin;
+        margin = _;
+        return chart;
     };
-  
+
     chart.width = function (_) {
-      if (!arguments.length) return width;
-      width = _;
-      return chart;
+        if (!arguments.length) return width;
+        width = _;
+        return chart;
     };
-  
+
     chart.height = function (_) {
-      if (!arguments.length) return height;
-      height = _;
-      return chart;
+        if (!arguments.length) return height;
+        height = _;
+        return chart;
     };
-  
+
     chart.x = function (_) {
-      if (!arguments.length) return xValue;
-      xValue = _;
-      return chart;
+        if (!arguments.length) return xValue;
+        xValue = _;
+        return chart;
     };
-  
+
     chart.y = function (_) {
-      if (!arguments.length) return yValue;
-      yValue = _;
-      return chart;
+        if (!arguments.length) return yValue;
+        yValue = _;
+        return chart;
     };
-  
+
     chart.xLabel = function (_) {
-      if (!arguments.length) return xLabelText;
-      xLabelText = _;
-      return chart;
+        if (!arguments.length) return xLabelText;
+        xLabelText = _;
+        return chart;
     };
-  
+
     chart.yLabel = function (_) {
-      if (!arguments.length) return yLabelText;
-      yLabelText = _;
-      return chart;
+        if (!arguments.length) return yLabelText;
+        yLabelText = _;
+        return chart;
     };
-  
+
     chart.yLabelOffset = function (_) {
-      if (!arguments.length) return yLabelOffsetPx;
-      yLabelOffsetPx = _;
-      return chart;
+        if (!arguments.length) return yLabelOffsetPx;
+        yLabelOffsetPx = _;
+        return chart;
     };
 
     chart.selectionDispatcher = function (_) {
-      if (!arguments.length) return dispatcher;
-      dispatcher = _;
-      return chart;
+        if (!arguments.length) return dispatcher;
+        dispatcher = _;
+        return chart;
     };
-  
+
     // Given selected data from another visualization 
     // select the relevant elements here (linking)
     chart.updateSelection = function (selectedData) {
-      if (!arguments.length) return;
-  
-      // Select an element if its datum was selected
-      selectableElements.classed('selected', d =>
-        selectedData.includes(d)
-      );
-  
+        if (!arguments.length) return;
+
+        // Select an element if its datum was selected
+        selectableElements.classed('selected', d =>
+            selectedData.includes(d)
+        );
+
     };
-  
+
     return chart;
-  }
+}
